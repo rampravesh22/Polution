78ebfb5fe0d8cd5285d7aba0f33dbefd7e823933
rpc <rpc7863@gmail.com>
2022-05-22 16:07:10 +0530

    drop down

--- Pollution/settings.py
+++ Pollution/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'livereload',
     "debug_toolbar",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
